b0e437c7691211ad7fdaf825f6a34b126b22aba8
Lazytrackz <lazytrackz@gmail.com>
2026-06-06 00:33:28 +0300

    Done

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,25 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Aleksey Kosichenko on 31.05.2026.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - ImagesListCell
+
+final class ImagesListCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    //TODO: добавить UIView с градиентом
+    
+    // MARK: - Properties
+    
+    static let reuseIdentifier = "ImagesListCell"
+}
